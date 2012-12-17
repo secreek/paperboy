307ae5c128d69b601e7ed91a978c3747e182f2b6
Void Main <voidmain1313113@gmail.com>
2012-12-17 21:00:55 +0800

    rescue the server from json parse error

--- src/paperboy.rb
+++ src/paperboy.rb
@@ -2,22 +2,35 @@
 require 'json'
 
 class Paperboy
+  attr_accessor :status, :message
+
   def call env
     request = Rack::Request.new env
     access_key_id = request['AccessKeyId']
     signature = request['Signature']
     request_data = env['rack.input'].read
-    request_json = JSON.load request_data
+    begin
+      request_json = JSON.load request_data
+    rescue
+      request_json = nil
+      @status = 400
+      @message = "Problems parsing JSON"
+    end
 
-    # Process & Send the message later
-    # access_key_id stores the AccessKeyId in protocol
-    # signature store the Signature in protocol
-    # request_json is the JSON object that stores the data
+    # TODO test access_key_id against signature
+
+    # Only process the message if request_json is not nil
+    if request_json
+      # Process & Send the message later
+      # access_key_id stores the AccessKeyId in protocol
+      # signature store the Signature in protocol
+      # request_json is the JSON object that stores the data
+    end
 
     Rack::Response.new.finish do |res|
       res['Content-Type'] = 'text/plain'
-      res.status = 200
-      str = "Hello, World"
+      res.status = @status
+      str = @message
       res.write str
     end
   end
