d4180f5fcb1de77cd14620c14b458ed5332ff9f4
Void Main <voidmain1313113@gmail.com>
2012-12-17 20:56:33 +0800

    parse AccessKeyId & Signature & JSON

--- src/paperboy.rb
+++ src/paperboy.rb
@@ -1,8 +1,18 @@
 # The class that processes the HTTP request and give the HTTP response
+require 'json'
 
 class Paperboy
   def call env
     request = Rack::Request.new env
+    access_key_id = request['AccessKeyId']
+    signature = request['Signature']
+    request_data = env['rack.input'].read
+    request_json = JSON.load request_data
+
+    # Process & Send the message later
+    # access_key_id stores the AccessKeyId in protocol
+    # signature store the Signature in protocol
+    # request_json is the JSON object that stores the data
 
     Rack::Response.new.finish do |res|
       res['Content-Type'] = 'text/plain'
